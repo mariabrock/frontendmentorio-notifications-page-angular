5514ef40918a4951d2ffc39c576aadf9216f7396
Maria Brock <brockm@ornl.giv>
2023-12-15 17:00:22 -0600

    added behavior subject

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, Output } from '@angular/core';
 import { ClearMessageService } from "../services/clear-message.service";
+import EventEmitter from "events";
 
 @Component({
   selector: 'app-header',
@@ -9,10 +10,19 @@ import { ClearMessageService } from "../services/clear-message.service";
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  @Output() readEvent = new EventEmitter();
+
   private messageService?: ClearMessageService;
+  public isRead = false;
 
   constructor() {
     this.messageService = inject(ClearMessageService);
+  }
+
+  emitBool() {
+    this.isRead = !this.isRead;
+    this.readEvent.emit(this.isRead.toString());
+    console.log(this.isRead);
   }
 
 }
--- src/app/services/clear-message.service.ts
+++ src/app/services/clear-message.service.ts
@@ -1,11 +1,13 @@
 import { Injectable, Output } from '@angular/core';
 import EventEmitter from "events";
+import { BehaviorSubject } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClearMessageService {
-  @Output() readEvent = new EventEmitter();
+  private boolSource = new BehaviorSubject<boolean>(false);
+  currentBool = this.boolSource.asObservable();
 
   public isRead = false;
 
@@ -16,4 +18,5 @@ export class ClearMessageService {
     this.readEvent.emit(this.isRead.toString());
     console.log(this.isRead);
   }
+
 }
